727eb769468d0d799c189b5a766297ada211a254
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-18 14:52:30 +0700

    Adding Content Section.

--- lib/main.dart
+++ lib/main.dart
@@ -19,129 +19,167 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 100, vertical: 30),
-        child: Column(
-          children: [
-            // NAVIGATION SECTION
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/background.png',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.fill,
+          ),
+          SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 100, vertical: 30),
+            child: Column(
               children: [
-                Image.asset(
-                  'assets/images/logo.png',
-                  width: 72,
-                  height: 40,
-                ),
+                // NAVIGATION SECTION
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
+                    Image.asset(
+                      'assets/images/logo.png',
+                      width: 72,
+                      height: 40,
+                    ),
+                    Row(
                       children: [
-                        Text(
-                          'Guide',
-                          style: GoogleFonts.poppins(
-                            fontSize: 18,
-                            color: Color(0XFF1D1E3C),
-                            fontWeight: FontWeight.w500,
-                          ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Guide',
+                              style: GoogleFonts.poppins(
+                                fontSize: 18,
+                                color: Color(0XFF1D1E3C),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Container(
+                              width: 66,
+                              height: 2,
+                              decoration: BoxDecoration(
+                                color: Color(0XFFFE998D),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ],
                         ),
-                        Container(
-                          width: 66,
-                          height: 2,
-                          decoration: BoxDecoration(
-                            color: Color(0XFFFE998D),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Pricing',
+                              style: GoogleFonts.poppins(
+                                fontSize: 18,
+                                color: Color(0XFF1D1E3C),
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            Container(
+                              width: 66,
+                              height: 2,
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Team',
+                              style: GoogleFonts.poppins(
+                                fontSize: 18,
+                                color: Color(0XFF1D1E3C),
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            Container(
+                              width: 66,
+                              height: 2,
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Stories',
+                              style: GoogleFonts.poppins(
+                                fontSize: 18,
+                                color: Color(0XFF1D1E3C),
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            Container(
+                              width: 66,
+                              height: 2,
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                    SizedBox(
-                      width: 50,
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'Pricing',
-                          style: GoogleFonts.poppins(
-                            fontSize: 18,
-                            color: Color(0XFF1D1E3C),
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        Container(
-                          width: 66,
-                          height: 2,
-                          decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 50,
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'Team',
-                          style: GoogleFonts.poppins(
-                            fontSize: 18,
-                            color: Color(0XFF1D1E3C),
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        Container(
-                          width: 66,
-                          height: 2,
-                          decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 50,
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'Stories',
-                          style: GoogleFonts.poppins(
-                            fontSize: 18,
-                            color: Color(0XFF1D1E3C),
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        Container(
-                          width: 66,
-                          height: 2,
-                          decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ],
+                    Image.asset(
+                      'assets/images/btn_account.png',
+                      width: 163,
+                      height: 53,
                     ),
                   ],
                 ),
+                // END OF NAVIGATION
+                SizedBox(
+                  height: 76,
+                ),
                 Image.asset(
-                  'assets/images/btn_account.png',
-                  width: 163,
-                  height: 53,
+                  'assets/images/illustration.png',
+                  height: 550,
+                ),
+                SizedBox(
+                  height: 84,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/icon_down.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(
+                      width: 13,
+                    ),
+                    Text(
+                      'Scroll to Learn More',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            // END OF NAVIGATION
-            SizedBox(
-              height: 76,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
